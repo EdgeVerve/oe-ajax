15c87565775b790b461c4c99d5e0cfac87982850
Rohit Khode <Rohit.Khode@ad.infosys.com>
2019-09-04 08:40:32 +0530

    allow intercepting request/response

--- oe-ajax.js
+++ oe-ajax.js
@@ -39,11 +39,17 @@ import "oe-utils/oe-utils.js";
 class OeAjax extends OECommonMixin(PolymerElement) {
   static get is() { return 'oe-ajax'; }
 
+  /** 
+   * Fired before a request is sent.
+   * Details contains 'request' and 'requestOptions' object.
+   * @event oe-ajax-presend
+   */
+
   /**
-     * Fired before a request is sent.
-     *
-     * @event oe-ajax-presend
-     */
+   * Fired after a response is received.
+   * Details is the 'request' object. 
+   * @event oe-ajax-postreceive
+   */
 
   /**
    * Fired when a request is sent.
@@ -530,7 +536,7 @@ class OeAjax extends OECommonMixin(PolymerElement) {
         request: request,
         options: requestOptions
       }, {
-        bubbles: this.bubbles,
+        bubbles: true,
         cancelable: true
       });
 
@@ -573,6 +579,10 @@ class OeAjax extends OECommonMixin(PolymerElement) {
   }
 
   _handleResponse(request) {
+    
+    /* Allow modification of response */
+    var evt = this.fire('oe-ajax-postreceive', request);
+
     if (request === this.lastRequest) {
       this._setLastResponse(request.response);
       this._setLastError(null);
